f85815801fefb76ab4e8280160556da674bf11ad
kuriakariuki <kariukijohnk1@gmail.com>
2022-03-22 06:48:44 -0700

    Added a description of _strlen

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * int_strlen - a function that returns the length of a string.
+ * _strlen - a function that returns the length of a string.
  * @s: The string
  * Return: The length of a string
  */
