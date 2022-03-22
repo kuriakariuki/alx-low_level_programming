41251051abe803ef773f9d20a8568c65fdb60109
kuriakariuki <kariukijohnk1@gmail.com>
2022-03-22 06:33:12 -0700

    Ceated file 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,12 +4,12 @@
  * int_strlen - a function that returns the length of a string.
  * @s: The string
  */
-void int_strlen(char *s)
+int int_strlen(char *s)
 {
 	int i;
 
 	i = 0;
-	while (s[i] != '/0')
+	while (s[i] != '\0')
 	{
 		i++;
 	}
