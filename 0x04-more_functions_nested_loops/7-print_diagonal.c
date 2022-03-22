669744e55f3f339618c553442f733bef188cda8e
kuriakariuki <kariukijohnk1@gmail.com>
2022-03-22 05:30:26 -0700

    Created file 0-reset_to_98

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,18 +11,6 @@ void print_diagonal(int n)
 	}
 	else
 	{
-		int i, j;
-
-		for (i = 1; i <= n; i++)
-		{
-			for (j = 0; j < n; j++)
-			{
-				if (j == i)
-					_putchar('\\');
-				else if (j < i)
-					_putchar(' ');
-			}
-			_putchar('\n');
-		}
+		
 	}
 }
---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - Take a pointer of an int as a parameter and updates the
+ * value it points to 98
+ * @n: Is the pointer to the variable to update
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
