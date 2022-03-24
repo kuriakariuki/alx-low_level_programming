418f35cd94e9934d6bcefc792b72dd17279d4c3a
KuriaKariuki <kariukijohnk1@gmail.com>
2022-03-24 21:53:40 +0300

    Created 0-strcat.c file

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,11 +13,11 @@ char *_strcat(char *dest, char *src)
 
 	i = 0;
 	j = 0;
-
 	while (dest[i] != '\0')
+	{
 		i++;
-	
-	while (src[j] != '\0')i
+	}
+	while (src[j] != '\0')
 	{
 		dest[i] = src[i];
 		j++;
