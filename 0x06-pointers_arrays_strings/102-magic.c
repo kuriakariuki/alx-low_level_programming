2939d4e8e8c8751d50eae10cf4f712f2209f2e88
KuriaKariuki <kariukijohnk1@gmail.com>
2022-03-25 19:09:00 +0300

    Created 102-magic.c file

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - prints a[2] = 98
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	int n;
+	int a[5];
+	int *p;
+
+	a[2] = 1024;
+	p = &n;
+	/*
+	 *write your line of code here
+	 *Remember:
+	 * - your are not allowed to use a
+	 * - you are not allowed to modify p
+	 * - only one statement
+	 * - you are not allowed to code anything else than this line of code
+	 */
+	*(p + 5) = 98;
+	/* this print 98\n */
+	printf("a[2] = %d\n", a[2]);
+	return (0);
+}
